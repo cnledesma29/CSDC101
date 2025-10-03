a9e7c862debe80182ca7aebd41d470c95a47fda2
ella <cnledesma@gbox.adnu.edu.ph>
2025-10-02 21:41:17 -0700

    Update WHILE LOOP.cpp

--- WHILE LOOP.cpp
+++ WHILE LOOP.cpp
@@ -15,3 +15,47 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 5;
+    
+    while(number > 0){
+        cout<< "LIKEY"<< endl;
+        number ++;
+
+    }
+    
+    
+
+    return 0;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 1;
+    char answer;
+    
+    do{
+        cout<< "Do you want to build a snowman? press y for yes and n for no" << endl;
+        cin>> answer;
+        cout<< "Sing let it go"<< endl;
+        
+    }
+    while (answer != 'n');
+    
+    cout<< "Ok byee" << endl;
+    return 0;
+}
