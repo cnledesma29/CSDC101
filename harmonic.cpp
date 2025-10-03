0a8796e93001cf798bba0dce757fcc8a7df96ce1
ella <cnledesma@gbox.adnu.edu.ph>
2025-10-02 23:45:58 -0700

    harmonic.cpp

---
+++ harmonic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int numerator, denominator = 1, n = 10;
+   
+   while (denominator <= n){
+        cout<< "1/" << denominator << ",";
+        denominator++;
+   }
+   
+    
+    
+    
+    return 0;
+    
+}
