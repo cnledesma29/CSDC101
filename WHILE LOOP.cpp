1d581f94c47393b43eddc211a82fca3e5d2dd2a3
ella <cnledesma@gbox.adnu.edu.ph>
2025-10-02 21:28:16 -0700

    WHILE LOOP.cpp

---
+++ WHILE LOOP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    
+    while(number < 5){
+        cout<< "LIKEY number: "<< number << endl;
+        number ++;
+
+    }
+    
+    
+
+    return 0;
+}
