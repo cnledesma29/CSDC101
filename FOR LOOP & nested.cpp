4aa0dd907dddae0ae6133e0a130d68790d187504
ella <cnledesma@gbox.adnu.edu.ph>
2025-10-02 22:11:49 -0700

    FOR LOOP & nested.cpp

---
+++ FOR LOOP & nested.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int start =0; start < 5; start ++){
+        cout<< "LIKEY NUMBER: "<< start << endl; 
+        
+    }
+}
+
+and
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 10; row ++){
+        
+   
+    for (int column = 1; column <= 10; column ++){
+        cout<< "*";
+    }
+    cout<< endl;
+    }
+    
+    return 0;
+    
+}
+
+and
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 10; row ++){
+        cout<< "row number: " << row << " "; 
+    
+    for (int column = 1; column <= 10; column ++){
+        cout<<column <<" ";
+    }
+    
+    cout<<endl;
+    }
+    
+    return 0;
+    
+}
+
+
