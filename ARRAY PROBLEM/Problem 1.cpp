a58b5ea0ca46b8b2f0c438447f6486269cb59409
ella <cnledesma@gbox.adnu.edu.ph>
2025-11-19 17:20:58 +0800

    Problem 1.cpp

---
+++ ARRAY PROBLEM/Problem 1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int n;
+  cout << "Enter number of elements: ";
+  cin >> n;
+
+ int number[n];
+ cout << "Enter elements: ";
+ for (int i = 0; i < n ; n++){
+ cin >> number[n];
+ }
+
+int yes = 1, no = 0;
+for (int i = 1; i < n - 1; i++){
+if (number[n] > number[n - 1]){
+no = 0;
+  }
+}
+
+if(yes == 1)
+  cout << "The elements is in ASCENDING order.";
+else
+  cout << "The element is NOT in ascending order.";
+
+return 0;
+
+}
